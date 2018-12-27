2a56016793356b4f6bf3503c4ce901e07d420264
Natan Cardoso <natancb7@gmail.com>
2018-12-27 12:36:16 -0300

    Configuração inicial do React

--- exercicios_webpack/ex/index.js
+++ exercicios_webpack/ex/index.js
@@ -4,7 +4,7 @@ import 'react'
 export default props => (
     // arrow functions ( => ) em ES6
     // implicitamente tem um return
-    { /*<h1> Olá, Mundo! </h1>*/ }
+    <h1> Olá, Mundo! </h1>
 )
 
 console.log('Funcionou! Uhu')
