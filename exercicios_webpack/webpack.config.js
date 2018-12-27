1df4305ce5eeab1a94b493098637a84865cca50d
Natan Cardoso <natancb7@gmail.com>
2018-12-26 22:08:46 -0300

    Configuração inicial do Webpack

---
+++ exercicios_webpack/webpack.config.js
@@ -0,0 +1,28 @@
+const webpack = require('webpack')
+    // referencia para o webpack
+
+module.exports = {
+    // objeto que configura toda a aplicação
+    // realização de transpiler
+    // para o node.js colocando exports ficará visivel fora desse arquivo
+    entry: './ex/index.js',
+    // ponto de entrada para que a aplicação seja carregada pelo webpack
+    output: {
+        // onde será disponibilizado o resultado
+        path: __dirname + '/public',
+        // dirname é um variavel de ambente do node.js
+        // para aponta para o diretório atual
+        filename: './bundle.js'
+            // ./ diretório relativo
+    },
+    devServer: {
+        // servidor focado para o desenvolvimento
+        // recarrega o browser quando modificado
+        // gera o bundle
+        port: 8080,
+        contentBase: './public'
+            // pasta base onde terá todos os arquivos a serem carregados
+            // dentro da pasta public haverá um index.html 
+            // que fará referência para o bundle, para testes de console
+    }
+}
